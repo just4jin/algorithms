bd286868063147dc71838265fbe20cb2764bff07
just4jin <just4jin@users.noreply.github.com>
2015-02-27 21:23:45 -0500

    Update rock_paper_scissors_lizard_spoke.py

--- mini_project/rock_paper_scissors_lizard_spoke.py
+++ mini_project/rock_paper_scissors_lizard_spoke.py
@@ -1,10 +1,11 @@
-# Rock-paper-scissors-lizard-Spock template
-
+# Rock-paper-scissors-lizard-Spock (RPSLS) is a variant of Rock-paper-scissors that allows five choices. 
+# Each choice wins against two other choices, loses against two other choices and ties against itself. 
+# Much of RPSLS's popularity is that it has been featured in 3 episodes of the TV series "The Big Bang Theory". 
 
 # The key idea of this program is to equate the strings
 # "rock", "paper", "scissors", "lizard", "Spock" to numbers
 # as follows:
-#
+
 # 0 - rock
 # 1 - Spock
 # 2 - paper
